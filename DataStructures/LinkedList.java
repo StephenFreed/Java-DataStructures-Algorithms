3f2667e3056d5ccc8dd4e1b29abddcea11752a90
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2021-10-21 10:02:06 -0400

    added LinkedList method

--- DataStructures/LinkedList.java
+++ DataStructures/LinkedList.java
@@ -1,10 +1,13 @@
 /*
-Implementation of a singly linked list
+implementation of a singly linked list
 node class for this linked list is below the LinkedList class
+many examples of methods you may want to implement
  */
 
 // comment out package to run file by itself
 package DataStructures;
+
+import java.util.Objects;
 
 class LinkedList {
 
@@ -92,6 +95,66 @@ class LinkedList {
         return (this.head == null);
     }
 
+    // swaps 2 elements in singly linked list based of node data
+    public void swapElements(String elm1, String elm2) {
+        // checks if list is empty
+        if (this.head == null) {
+            System.out.println("This linked list is empty.");
+            return;
+        }
+        // checks if elm to be switched are the same element
+        else if (Objects.equals(elm1, elm2)) {
+            System.out.println("Both elements are the same, no need to swap.");
+            return;
+        } else {
+            // printing out what elements we are swapping
+            System.out.println("Swapping " + elm1 + " with " + elm2 + " in linked list.");
+        }
+        // finding node with first elm parameter as nodes data
+        SinglyLinkedNode node1 = this.head;
+        SinglyLinkedNode node1Prev = null;
+        while (node1 != null) {
+            if (Objects.equals(node1.data, elm1)) {
+                break;
+            }
+            node1Prev = node1;
+            node1 = node1.getNextNode();
+        }
+        // finding node with second elm parameter as nodes data
+        SinglyLinkedNode node2 = this.head;
+        SinglyLinkedNode node2Prev = null;
+        while (node2 != null) {
+            if (Objects.equals(node2.data, elm2)) {
+                break;
+            }
+            node2Prev = node2;
+            node2 = node2.getNextNode();
+        }
+        // if node1 was head sets node2 to head, if not sets node1Prev next node to node2
+        if (Objects.equals(node1Prev, null)) {
+            this.head = node2;
+        } else {
+            node1Prev.setNextNode(node2);
+        }
+        // if node2 was head sets node1 to head, if not sets node2Prev next node to node1
+        if (Objects.equals(node2Prev, null)) {
+            this.head = node1;
+        } else {
+            node2Prev.setNextNode(node1);
+        }
+        // checks if either node1 or node2 are null
+        if (Objects.equals(node1, null) || Objects.equals(node2, null)) {
+            System.out.println("One of the data elements are not in the linked list.");
+        // sets node1 to node2 next node, sets node2 to node1 next node
+        } else {
+            SinglyLinkedNode node1NextNode = node1.getNextNode();
+            SinglyLinkedNode node2NextNode = node2.getNextNode();
+            node1.setNextNode(node2NextNode);
+            node2.setNextNode(node1NextNode);
+        }
+
+    }
+
     // prints linked list from head to tail
     public void printList() {
         // sets string output variable
@@ -139,8 +202,14 @@ class LinkedList {
         test.printList();
         System.out.println("Is this list empty? " + test.isEmpty());
         System.out.println();
-        
+
         test.removeHead();
+        test.printList();
+        System.out.println();
+
+        test.addToHead("A");
+        test.printList();
+        test.swapElements("A","C");
         test.printList();
 
     }
