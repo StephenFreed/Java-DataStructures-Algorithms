89a607398d544d3b81d1e4de9179bfb06b350392
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2021-10-28 15:06:17 -0400

    add FindPrimeNumbers

---
+++ Algorithms/FindPrimeNumbers.java
@@ -0,0 +1,38 @@
+package Algorithms;
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+public class FindPrimeNumbers {
+
+    public static void main(String[] args) {
+
+        // gets user input on number to find prime numbers up to
+        System.out.print("Enter Number. Find Prime Numbers Up To: ");
+        Scanner scanner = new Scanner(System.in);
+        int primesToFind = scanner.nextInt();
+
+        List<Integer> primeNumbers = new ArrayList<>();
+
+        // loops through the numbers up to the number input
+        for (int numberToCheck = 2; numberToCheck <= primesToFind; numberToCheck++) {
+            // assume we have a prime number
+            boolean isPrime = true;
+            // factor to divide each number up to half the number
+            for (int factor = 2; factor <= numberToCheck/2; factor++) {
+                if (numberToCheck % factor == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                primeNumbers.add(numberToCheck);
+            }
+        }
+        System.out.println("List Of Prime Numbers From 0 To " + primesToFind);
+        for (int primes : primeNumbers) {
+            System.out.print(primes + ", ");
+        }
+    }
+}
