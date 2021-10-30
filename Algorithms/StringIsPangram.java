6a5717fd7efc36b2dc3207558a19cadda7b9a472
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2021-10-30 13:22:38 -0400

    add StringIsPangram

---
+++ Algorithms/StringIsPangram.java
@@ -0,0 +1,35 @@
+/*
+check if string is pangram
+pangram contains all characters of the alphabet
+ */
+
+package Algorithms;
+
+public class StringIsPangram {
+
+    private static boolean isPangram(String string) {
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            // uppercase value of ch
+            char chUpperCase = Character.toUpperCase(ch);
+            // checks if character is in string(upper and lowercase)
+            if (string.indexOf(ch) < 0 && string.indexOf(chUpperCase) < 0) {
+                return false;
+            }
+        }
+        // if loop makes it through all characters true
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String testOne =  "Pack my box with five dozen liquor jugs."; // should be true
+        System.out.println(isPangram(testOne));
+
+        String testTwo =  "Glib jocks quiz nymph to vex dwarf."; // should be true
+        System.out.println(isPangram(testTwo));
+
+        String testThree =  "bcd efgHIJk lmnop qrSTU VwxyZ"; // should be false
+        System.out.println(isPangram(testThree));
+
+    }
+}
