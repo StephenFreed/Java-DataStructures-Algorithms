ca712ace929c06998dfd03c2e42fa1d1b2758a52
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2021-10-31 15:35:38 -0400

    add AlphabetShift

---
+++ Algorithms/AlphabetShift.java
@@ -0,0 +1,32 @@
+/*
+given a string of lowercase letters and a key to shift those letters over by that amount
+return new string
+ */
+
+package Algorithms;
+
+public class AlphabetShift {
+
+    public static String returnShiftedString(String str, int key) {
+        // create char[] length of str
+        // create alphabet string to index off
+        // iterate through length of string getting shifted index of char in alphabet
+
+        char[] answer = new char[str.length()];
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        for (int i = 0; i < str.length(); i++) {
+            int currentIdx = alphabet.indexOf(str.charAt(i));
+            int wrappedIdx = (currentIdx + key) % 26;
+            answer[i] = alphabet.charAt(wrappedIdx);
+        }
+
+        return String.valueOf(answer);
+    }
+
+    public static void main(String[] args) {
+        String shiftTest = "stu";
+        int keyTest = 62;
+        System.out.println(returnShiftedString(shiftTest, keyTest));
+    }
+}
