a0f6faaee01f34b26ac013b6ea452ed7d669d319
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2021-11-02 11:48:16 -0400

    memoization in NthFibonacci

--- Algorithms/NthFibonacci.java
+++ Algorithms/NthFibonacci.java
@@ -1,5 +1,8 @@
 package Algorithms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NthFibonacci {
 
     public static int recursion(int n) {
@@ -9,29 +12,42 @@ public class NthFibonacci {
         if (n == 1) {
             return 0;
         }
-        return recursion(n-1) +recursion(n-2);
+        return recursion(n-1) + recursion(n-2);
 
     }
 
     public static int memoization(int n) {
-        return -1;
+        Map<Integer,Integer> hashMap = new HashMap<>();
+        hashMap.put(2,1);
+        hashMap.put(1,0);
+        return memoization(n, hashMap);
+    }
+
+    public static int memoization(int n, Map<Integer,Integer> hashMap) {
+        if (hashMap.containsKey(n)) {
+            return hashMap.get(n);
+        }
+        else {
+            hashMap.put(n, memoization(n - 1, hashMap) + memoization(n-2, hashMap));
+            System.out.println(hashMap);
+            return hashMap.get(n);
+        }
     }
 
     public static int iterating(int n) {
         int[] prevTwo = {0,1};
-        int counter = 3;
 
-        while (counter <= n) {
+        for (int nthFib = 3; nthFib <= n; nthFib++) {
             int nextFib = prevTwo[0] + prevTwo[1];
             prevTwo[0] = prevTwo[1];
             prevTwo[1] = nextFib;
-            counter++;
         }
         return n > 1 ? prevTwo[1] : prevTwo[0];
     }
 
     public static void main(String[] args) {
         System.out.println(recursion(5));
-        System.out.println(iterating(5));
+        System.out.println(iterating(12));
+        System.out.println(memoization(12));
     }
 }
