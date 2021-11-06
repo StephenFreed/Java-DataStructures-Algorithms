9640bc3ca5c7fbc07f05306e70f85d863705357a
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2021-11-05 21:05:13 -0400

    add file

---
+++ DataStructures/TimeComparison.java
@@ -0,0 +1,71 @@
+package DataStructures;
+
+/*
+test speed of different data structures with same operation
+ */
+
+import java.util.*;
+
+public class TimeComparison {
+
+    public static void main(String[] args) {
+        // data structures
+        List<Integer> linkedList = new LinkedList<>();
+        List<Integer> arrayList = new ArrayList<>();
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        // add to structures
+        for (int i = 0; i < 100_000; i++) {
+            linkedList.add(i);
+            arrayList.add(i);
+            deque.add(i);
+        }
+
+        long startTime;
+        long endTime;
+        long elapsedTime;
+
+        int number = 0;
+
+        // =========== LinkedList ===========
+        startTime = System.nanoTime();
+
+        // do something
+        linkedList.remove(number);
+
+        endTime = System.nanoTime();
+
+        elapsedTime = endTime - startTime;
+        System.out.println("LinkedList: " + elapsedTime);
+        // =========== LinkedList ===========
+
+
+        // =========== ArrayList  ===========
+        startTime = System.nanoTime();
+
+        // do something
+        arrayList.remove(number);
+
+        endTime = System.nanoTime();
+
+        elapsedTime = endTime - startTime;
+        System.out.println("ArrayList: " + elapsedTime);
+        // =========== LinkedList ===========
+
+
+        // =========== Deque  ===========
+        startTime = System.nanoTime();
+
+        // do something
+        deque.remove();
+
+        endTime = System.nanoTime();
+
+        elapsedTime = endTime - startTime;
+        System.out.println("Deque: " + elapsedTime);
+        // =========== LinkedList ===========
+
+
+    }
+
+}
