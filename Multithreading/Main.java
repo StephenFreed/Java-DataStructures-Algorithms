3329f2ca3700a072e39ccaa08f2fd24230ef3d5c
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2022-01-01 13:52:04 -0500

    Multithreading Example

---
+++ Multithreading/Main.java
@@ -0,0 +1,17 @@
+package Multithreading; 
+
+public class Main {
+
+    public static void main(String[] args) {
+        // first way to create thread
+        MyThread thread1 = new MyThread();
+
+        // second way to create thread
+        MyRunnable runnable1 = new MyRunnable();
+        Thread thread2 = new Thread(runnable1);
+
+        thread1.start();
+        thread2.start();
+    }
+
+}
